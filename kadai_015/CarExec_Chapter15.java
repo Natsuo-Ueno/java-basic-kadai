c6b14fa321b487f0f10ca80134f3e1a278008f64
Natsuo-Ueno <b3cam972@gmail.com>
2025-12-04 22:00:32 +0900

    kadai_15を修正

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -3,11 +3,11 @@ package kadai_015;
 public class CarExec_Chapter15 {
 	public static void main(String[] args) {
 		
-		Car_Chapter15 kadai_15 = new Car_Chapter15(1,10);
+		Car_Chapter15 car = new Car_Chapter15();
 		
-		kadai_15.changeGear(3);
+		car.changeGear(3);
 		
-		kadai_15.run();
+		car.run();
 	}
 
 }
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,35 +1,29 @@
 package kadai_015;
 
 public class Car_Chapter15 {
-	private int gear  = 0; //ギア
-	private int speed = 0; //速度
-	
-	//コンストラクタ
-	public Car_Chapter15(int gear, int speed) {
-		this.gear = gear;
-		this.speed = speed;
-	}
-	
+	private int gear  = 1; //ギア
+	private int speed = 10; //速度	
 	
 	//【メソッド】ギアの値により速度を変える
-	public void changeGear(final int afterGear) {
+	public void changeGear(int afterGear) {
 		
 		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
 		
-		this.speed = switch(afterGear) {
+		this.gear = afterGear;
+		
+		this.speed = switch(this.gear) {
 			case 1 -> 10;
 			case 2 -> 20;
 			case 3 -> 30;
 			case 4 -> 40;
 			case 5 -> 50;
-		   default -> 60;
+		   default -> 10;
 		};
 		
 	}
 	
 	//【メソッド】ギアチェンジ後の速度を表示する
 	public void run() {
-			
 		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 
