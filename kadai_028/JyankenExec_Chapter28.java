da7e4333255cb061d6debfdd69d1f1e3108fe3bb
Natsuo-Ueno <b3cam972@gmail.com>
2025-12-28 12:42:19 +0900

    修正点を修正
    Jyanken_Chapter28.javaの変更点
    ・46行目Math.random()をMath.floor()に変更
    ・getMyChoiceメソッドにscanner.close()を追加
    ・playGameメソッドを引数を渡す形に変更
    ・playGameメソッドの勝敗判定のメッセージを修正
    
    JyankenExec_Chapter28.java
    ・getMyChoiceメソッド、getRandomメソッドの戻り値をplayGameメソッドの引数に渡す形に変更

--- kadai_028/JyankenExec_Chapter28.java
+++ kadai_028/JyankenExec_Chapter28.java
@@ -6,14 +6,8 @@ public class JyankenExec_Chapter28 {
 		//インスタンスの作成
 		Jyanken_Chapter28 Jyanken = new Jyanken_Chapter28();
 		
-		//getMyChoiceメソッドの実行
-		Jyanken.getMyChoice();
-		
-		//getRandomメソッドの実行
-		Jyanken.getRandom();
-		
-		//playGameメソッドの実行
-		Jyanken.playGame();
+		//getMyChoiceメソッド、getRandomメソッドの戻り値を引数に渡してplayGameメソッドを実行
+		Jyanken.playGame(Jyanken.getMyChoice(), Jyanken.getRandom());
  		 
 	}
 
--- kadai_028/Jyanken_Chapter28.java
+++ kadai_028/Jyanken_Chapter28.java
@@ -13,13 +13,13 @@ public class Jyanken_Chapter28 {
 	//自分のじゃんけんの手を入力するメソッド
 	public String getMyChoice() {
 		
+		//Scannerクラスのオブジェクトを生成する
+		Scanner scanner = new Scanner(System.in);
+		
 		for(int i = 1; i <= 3; i++) {
 	
 			//メッセージを表示
 			System.out.println("自分のじゃんけんの手を入力しましょう\nグーはrockのrを入力しましょう\nチョキはscissorsのsを入力しましょう\nパーはpaperのpを入力しましょう");
-		
-			//Scannerクラスのオブジェクトを生成する
-			Scanner scanner = new Scanner(System.in);
 			
 			//入力した内容を取得する
 			myChoice = scanner.nextLine();
@@ -27,27 +27,28 @@ public class Jyanken_Chapter28 {
 			//入力した内容が正しいか判定する
 			for(String hand : handArray) {
 				if(myChoice.equals(hand)) {
-					//入力内容を返す
-					return myChoice;
+					scanner.close();//scannerクラスを閉じる
+					return myChoice;//入力内容を返す
 				}
 			}
-			
 			//誤った入力だった場合の出力メッセージ
 			System.out.println("入力内容が正しくありません。r、s、pのいずれかを入力してください");
 		}
+		//誤った入力が3回行われた場合の出力メッセージ
 		System.out.println("誤った入力回数が3回に達したため強制終了します");
-		System.exit(0);
+		scanner.close();//scannerクラスを閉じる
+		System.exit(0);//プログラムを終了する
 		return null;
 	}		
 	
 	//対戦相手のじゃんけんの手を乱数で選ぶメソッド
 	public String getRandom() {
-		opponentChoice = handArray[(int) Math.round(Math.random() * 2)];
+		opponentChoice = handArray[(int) Math.floor(Math.random() * 3)];
 		return opponentChoice;
 	}
 	
 	//じゃんけんを行うメソッド
-	public void playGame() {
+	public void playGame(String myHand, String opponentHand) {
 		
 		//HashMapの宣言
 		HashMap<String,String> handMap = new HashMap<String,String>();
@@ -58,7 +59,7 @@ public class Jyanken_Chapter28 {
 		handMap.put("p", "パー");
 		
 		//じゃんけんの手を出力
-		System.out.println("自分の手は" + handMap.get(myChoice) + ",対戦相手の手は" + handMap.get(opponentChoice));
+		System.out.println("自分の手は" + handMap.get(myHand) + ",対戦相手の手は" + handMap.get(opponentHand));
 		
 		//HashMapの宣言
 		HashMap<String,Integer> handNumMap = new HashMap<String,Integer>();
@@ -69,12 +70,12 @@ public class Jyanken_Chapter28 {
 		handNumMap.put("p", 3);
 		
 		//じゃんけんの手を比較する
-		if(myChoice.equals(opponentChoice)) {
-			System.out.println("引き分けです");	
-		} else if((handNumMap.get(myChoice) - handNumMap.get(opponentChoice) == -1) || (handNumMap.get(myChoice) - handNumMap.get(opponentChoice) == 2)) {
+		if(myHand.equals(opponentHand)) {
+			System.out.println("あいこです");	
+		} else if((handNumMap.get(myHand) - handNumMap.get(opponentHand) == -1) || (handNumMap.get(myHand) - handNumMap.get(opponentHand) == 2)) {
 			System.out.println("自分の勝ちです");
 		} else {
-			System.out.println("相手の勝ちです");
+			System.out.println("自分の負けです");
 		}
 	}
 	
